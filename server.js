975832adcf3a03e58a6b2c15a6c426f770d820dd
thuyamyint <thuyamyint2022@gmail.com>
2025-07-14 11:42:00 +0630

    commit

--- server.js
+++ server.js
@@ -15,7 +15,9 @@ const io = new Server(server, {
     cors: {
         origin: [
             "http://localhost:3000",
-            "http://localhost:5173"
+            "http://localhost:5173",
+            "https://quick-serve-client.vercel.app",
+            "https://quick-serve-admin.vercel.app",
         ],
         methods: ["GET", "POST"]
     }
