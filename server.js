bf661037879fd4db471e8b2fbe342a1093f11c89
Thuya-Myint <165346326+Thuya-Myint@users.noreply.github.com>
2025-07-21 11:08:54 +0630

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 const allowedOrigins = [
     "http://localhost:3000",
+    "http://localhost:3001",
     "http://localhost:5173",
     "https://quick-serve-client.vercel.app",
     "https://quick-serve-admin.vercel.app",
