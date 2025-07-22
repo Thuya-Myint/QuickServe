8a90ade821d1f4d2037b949d3201a03824057e38
thuyamyint <thuyamyint2022@gmail.com>
2025-07-22 10:57:02 +0630

    reduce security and allow any endpoint to connect

--- server.js
+++ server.js
@@ -57,15 +57,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
+    origin: '*',
+    credentials: false
 }));
+
 
 app.use("/api/v1", require("./src/routes"));
 app.use(validateRequest)
